3bfbb39b04f3fa2843598cd0afc8e5930c647d70
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-06 10:09:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ZUgzFaZD#uJa01Fm-xcAGc_gaHb8KWeXuoquEq_Qv0xxkXY1XW24",
+SESSION_ID: process.env.SESSION_ID || "7U51FBwR#blyUEQZLxUVcsOpals7nZXH0EbDVaPoN1nMB0najT_A",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 };
